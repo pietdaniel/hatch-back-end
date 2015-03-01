d76fab6425a26b5884c213ed7ffdc72a50645467
pietdaniel <dpiet@salsify.com>
2015-03-01 12:16:13 -0500

    refactoring oauth flow

---
+++ neuhatch/utils.py
@@ -0,0 +1,31 @@
+import tweepy, sys, json
+from flask.ext.login import logout_user, logout_user, login_required, current_user
+from neuhatch import config, app, db, login_manager
+
+def get_base_auth():
+    return tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
+
+def verify_api(request_token, verifier):
+    """
+      Returns (username, key, secret)
+    """
+    auth = get_base_auth()
+    # set request token
+    auth.request_token = request_token
+    # turn request token + verifier into access token + secret
+    auth.get_access_token(verifier)
+    key = auth.access_token
+    secret = auth.access_token_secret
+    auth.set_access_token(key, secret)
+    # get api
+    api = tweepy.API(auth)
+    # return username, key, secret
+    username = api.me().screen_name
+    return (username, key, secret)
+
+def get_user_api(user):
+    auth = get_base_auth()
+    key = user.access_token
+    secret = user.access_token_secret
+    auth.set_access_token(key, secret)
+    return tweepy.API(auth)
--- neuhatch/views.py
+++ neuhatch/views.py
@@ -4,13 +4,14 @@ from flask.ext.login import logout_user, login_user, login_required, current_use
 from neuhatch import config, app, db, login_manager
 from neuhatch.models import User
 from neuhatch.crossdomain import crossdomain
+import neuhatch.utils as utils
 
 @login_manager.user_loader
 def load_user(userid):
     return User.query.filter_by(id=userid).first()
 
-
 @app.route("/users")
+@login_required
 def users():
     users = User.query.all()
     out = []
@@ -22,42 +23,43 @@ def users():
 @app.route("/oauth")
 def oauth():
     try:
+        # TODO: abstract callback url to config
         callback_url = "%s%s" % ("http://localhost:5000", url_for("callback"))
-        auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret, callback_url)
+        # create initial tweepy callback object
+        auth = utils.get_base_auth()
+        # get redirect url
         redirect_url = auth.get_authorization_url()
+        # set request token
         session['request_token'] = auth.request_token
         return redirect(redirect_url)
     except tweepy.TweepError as e:
         return 'Failed to get request token %s' % e
     except:
+        # todo error handling
         return str(sys.exc_info())
-
 
 @app.route('/callback')
 def callback():
     try:
-        auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
-        token = session['request_token']
-        del session['request_token']
-        auth.request_token = token
+        request_token = session['request_token']
         verifier = request.args.get('oauth_verifier')
+        del session['request_token']
     except:
         print("error getting auth verifier or request_token")
+        # todo appropriate error handling
         return str(sys.exc_info())
 
-    auth.get_access_token(verifier)
-    key = auth.access_token
-    secret = auth.access_token_secret
-    auth.set_access_token(key, secret)
+    username, key, secret = utils.verify_api(request_token, verifier)
 
-    api = tweepy.API(auth)
-    username = api.me().screen_name
     user = User.query.filter_by(username=username).first()
+
+    # create user if first time
     if user is None:
         user = User(username, key, secret)
         print("Creating user %s: %s" % (username, user,))
         db.session.add(user)
         db.session.commit()
+
     print("Logging in user %s" % user)
     login_user(user)
 
@@ -69,24 +71,18 @@ def callback():
 def user():
     return str((current_user.id, current_user.access_token, current_user.access_token_secret))
 
-
 @app.route('/search')
 @crossdomain(origin='*')
 @login_required
 def search():
+    # TODO: used logged in user
     user = User.query.filter_by(username="test").first()
-
-    auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
-    key = auth.access_token
-    secret = auth.access_token_secret
-    auth.set_access_token(key, secret)
-    api = tweepy.API(auth)
-
+    api = utils.get_user_api(user)
     query = request.args.get('q')
-
     results = api.search(q=query)
     output = []
     for result in results:
         output.append(result._json)
     out_json = json.dumps(output)
+
     return Response(out_json, mimetype="application/json")
