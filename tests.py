66fbf5e8389bc2b855524de85d79eb65da79a62a
Ali Ukani <ali.ukani@gmail.com>
2015-03-16 22:44:45 -0400

    Add test to show search page requires login

--- tests.py
+++ tests.py
@@ -21,6 +21,7 @@ class HatchTestCase(TestCase):
 
 class OAuthTests(HatchTestCase):
     """Test cases for our application's OAuth flow."""
+
     def test_no_default_authenticated_user(self):
         with app.test_request_context('/oauth'):
             assert current_user.is_anonymous()
@@ -29,3 +30,10 @@ class OAuthTests(HatchTestCase):
         response = self.client.get('/oauth')
         self.assertTrue(
             response.location.startswith("https://api.twitter.com/oauth"))
+
+
+class SearchTests(HatchTestCase):
+    def test_search_page_is_not_accessible_when_logged_out(self):
+        app.config['LOGIN_DISABLED'] = False
+        response = self.client.get('/search')
+        self.assert401(response)
