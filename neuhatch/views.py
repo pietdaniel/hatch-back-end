a3d0fe5edaf80682fa179a94d46952ed64218eb5
DJ Hartman <danieljayhartman@gmail.com>
2015-04-27 00:02:59 -0400

    Add default param and typecasting.

--- neuhatch/views.py
+++ neuhatch/views.py
@@ -131,11 +131,7 @@ def search():
         max_results -- Integer, the most tweets to return
     """
     query = request.args.get('q')
-
-    if request.args.get('max_results') is None:
-        max_results = 100
-    else:
-        max_results = float(request.args.get('max_results'))
+    max_results = request.args.get('max_results', default=100, type=int)
 
     return utils.json_response([
         tweet._json for tweet in search_for_tweets(query, max_results)
@@ -148,6 +144,8 @@ def search():
 def search_csv():
     """Return a CSV export of a search query."""
     query = request.args.get('q')
+    max_results = request.args.get('max_results', default=100, type=int)
+
     # TODO: write rows directly to the response (instead of to StringIO)
     stringbuffer = StringIO()
     fieldnames = [
